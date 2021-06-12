488648bbec68a075a34fa35af4f99dcbb32d9d1a
Sanskaar717 <73640122+Sanskaar717@users.noreply.github.com>
2021-06-12 11:04:08 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -27,9 +27,19 @@ class Game{
             form = new Form();
             form.display();
         }
+
+        car1 = createSprite(100,200);
+        car1.addImage(c1);
+        car2 = createSprite(300,200);
+        car2.addImage(c2);
+        car3 = createSprite(500,200);
+        car3.addImage(c3);
+        car4 = createSprite(700,200);
+        car4.addImage(c4);
+        cars = [car1,car2,car3,car4]; 
     }
 
-play(){
+    play(){
         form.hide();
         textSize(25);
         text("Game Start",120,100);
@@ -37,10 +47,26 @@ play(){
 
         if(allPlayers !== undefined){
             var display_position = 130;
+
+            background(100);
+            image(tJ,0,-displayHeight*4,displayWidth,displayHeight*5);
+            var index = 0;
+            var x = 175,y;
+
             for(var plr in allPlayers){
-                if(plr === "player" + player.index)
-                fill("red");
-                else
+
+                index = index + 1;
+                x = x + 200;
+                y = displayHeight-allPlayers[plr].distance;
+
+                cars[index - 1].x = x;
+                cars[index - 1].y = y;
+
+                if(index===player.index){
+                    cars[index - 1].shapeColor = "red";
+                    camera.position.x = displayWidth/2;
+                    camera.position.y = cars[index - 1].y;
+                }
                 fill(0);
                 display_position += 20
                 textSize(15);
@@ -48,8 +74,20 @@ play(){
             }
         }
 
-            if(keyIsDown("up") && player.index !==null){
-                player.distance +=50;
-                player.update();
-            }
-}}
+        if(keyIsDown("up") && player.index !==null){
+            player.distance +=50;
+            player.update();
+        }
+
+        if(player.distance>4000){
+            gameState = 2;
+        }
+
+        drawSprites();
+    }
+
+    end(){
+        console.log("Game Ended");
+    }
+
+}
