8b42abc758a92c571487e82d8503080c80ee255e
Sanskaar717 <73640122+Sanskaar717@users.noreply.github.com>
2021-08-15 10:59:23 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -27,20 +27,51 @@ class Game{
             form = new Form();
             form.display();
         }
+
+        car1 = createSprite(100,200);
+        car1.addImage(c1);
+        car2 = createSprite(300,200);
+        car2.addImage(c2);
+        car3 = createSprite(500,200);
+        car3.addImage(c3);
+        car4 = createSprite(700,200);
+        car4.addImage(c4);
+        cars = [car1,car2,car3,car4]; 
     }
 
-play(){
+    play(){
         form.hide();
         textSize(25);
         text("Game Start",120,100);
+        
         Player.getPlayerInfo();
+        player.getCarsAtEnd();
 
         if(allPlayers !== undefined){
             var display_position = 130;
+
+            background(100);
+            image(tJ,0,-displayHeight*4,displayWidth,displayHeight*5);
+            var index = 0;
+            var x = 175,y;
+
             for(var plr in allPlayers){
-                if(plr === "player" + player.index)
-                fill("red");
-                else
+
+                index = index + 1;
+                x = x + 200;
+                y = displayHeight-allPlayers[plr].distance;
+
+                cars[index - 1].x = x;
+                cars[index - 1].y = y;
+
+                if(index===player.index){
+                    stroke(10);
+                    fill("red");
+                    ellipse(x,y,60,60);
+                    cars[index - 1].shapeColor = "red";
+                    camera.position.x = displayWidth/2;
+                    camera.position.y = cars[index - 1].y;
+                }
                 fill(0);
                 display_position += 20
                 textSize(15);
@@ -48,8 +79,23 @@ play(){
             }
         }
 
-            if(keyIsDown("up") && player.index !==null){
-                player.distance +=50;
-                player.update();
-            }
-}}
+        if(keyIsDown(UP_ARROW) && player.index !==null){
+            player.distance +=50;
+            player.update();
+        }
+
+        if(player.distance>3800){
+            gameState = 2;
+            player.rank +=1;
+            Player.updateCarsAtEnd(player.rank);
+        }
+
+        drawSprites();
+    }
+
+    end(){
+        console.log("Game Ended");
+        console.log(player.rank);
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
 var gameState = 0, playerCount, database;
 var form, player, game, allPlayers;
+var car1, car2, car3, car4, cars;
+var c1, c2, c3, c4, t1, tJ, g;
+
+function preload(){
+    c1 = loadImage("car1.png");
+    c2 = loadImage("car2.png");
+    c3 = loadImage("car3.png");
+    c4 = loadImage("car4.png");
+    t1 = loadImage("track.png");
+    tJ = loadImage("track.jpg");
+    g = loadImage("ground.png");
+}
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(displayWidth-20,displayHeight-30);
     database = firebase.database();
 
     game = new Game();
@@ -18,5 +30,9 @@ function draw(){
     if(gameState===1){
         clear();
         game.play();
+    }
+
+    if(gameState===2){
+        game.end();
     }
 }
